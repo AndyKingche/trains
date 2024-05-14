c874e222680bccb538e3e02847bc6bfb6a1e4a16
Andy Quinche <laqm_14@hotmail.com>
2024-05-14 08:38:58 -0500

    feature: se ha construido el grafo y se ha realizado los diferentes caminos del tren

--- TrainsProblem1/src/Grafo.java
+++ TrainsProblem1/src/Grafo.java
@@ -1,4 +1,36 @@
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class Grafo {
 
+	private Map<Character, Map<Character, Integer>> rrecorrido = new HashMap<>();
+	
+	public void agergarArista(char inicio, char fin, int distancia) {
+		rrecorrido.putIfAbsent(inicio, new HashMap<>());
+		rrecorrido.get(inicio).put(fin, distancia);
+		
+	}
+	
+	public int obtenerDistancia(List<Character> camino) {
+		
+		int distancia = 0;
+		
+		for(int i = 0; i<camino.size()-1; i++ ) {
+			char inicio = camino.get(i);
+			
+			char fin = camino.get(i + 1);
+			
+			if(!rrecorrido.containsKey(inicio)|| !rrecorrido.get(inicio).containsKey(fin)) {
+				return -1;
+			}
+			
+			distancia += rrecorrido.get(inicio).get(fin);
+					
+			
+	}
+		
+		return distancia;
+	}
+	
 }
--- TrainsProblem1/src/TrainsProblem1.java
+++ TrainsProblem1/src/TrainsProblem1.java
@@ -1,8 +1,39 @@
+import java.util.Arrays;
+import java.util.List;
 
 public class TrainsProblem1 {
 
 	public static void main(String[] args) {
-		System.out.println("Hola");
+		
+		Grafo grafo = new Grafo();
+		
+		grafo.agergarArista('A', 'B', 5);
+		grafo.agergarArista('B', 'C', 4);
+		grafo.agergarArista('C', 'D', 8);
+		grafo.agergarArista('D', 'C', 8);
+		grafo.agergarArista('D', 'E', 6);
+		grafo.agergarArista('A', 'D', 5);
+		grafo.agergarArista('C', 'E', 2);
+		grafo.agergarArista('E', 'B', 3);
+		grafo.agergarArista('A', 'E', 7);
+		
+		List<List<Character>> camino = Arrays.asList(
+				
+				Arrays.asList('A','B','C'),
+				Arrays.asList('A','D'),
+				Arrays.asList('A','D','C'),
+				Arrays.asList('A','E','B','C','D'),
+				Arrays.asList('A','E','D')
+				);
+		
+		for(int i=0; i < camino.size(); i++) {
+			
+			int result = grafo.obtenerDistancia(camino.get(i));
+			
+			System.out.println("OUTPUT "+(i+1)+": "+(result == -1 ? "NO SUCH ROUTE" : result));
+		}
+				
+		
 
 	}
 
